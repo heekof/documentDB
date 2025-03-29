b718245aac6c9d724e2def7f5748ffe09f5fc165
Jaafar Bendriss <bendrisj2@gmail.com>
2025-03-29 13:44:30 +0100

    First example

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import uuid
+import json
+import os
+
+# TODO : check if file exist before creation
+# TODO : manage collections creation etc
+
+
+
 class SimpleDocumentDB:
     def __init__(self, db_path="mydb"):
         ...
@@ -7,7 +16,9 @@ class SimpleDocumentDB:
 
     def insert_one(self, collection, document):
         """
-        if collection in collections, check document and then add it.
+        if collection in collections, check document and then add it. otherwise create it.
+
+
         """
         ...
 
@@ -24,3 +35,33 @@ db = SimpleDocumentDB()
 
 db.insert_one("users", {"name": "Jaafar", "age": 34})
 results = db.find("users", {"age": 34})
+
+
+
+
+def get_random_uuid():
+    return str(uuid.uuid4())
+
+random_uuid = get_random_uuid()
+print(random_uuid)
+
+
+
+def create_json_file_based_on_name(file_name, source_db='mydb', collection_name=None):
+    
+    if collection:
+        storage_path = f"{source_db}/{collection_name}/{file_name}.json"
+    else:
+        storage_path = f"{source_db}/{file_name}.json"
+
+    data = {
+        "message": "This is a JSON file created based on the file name.",
+        "file_name": file_name
+        }
+    
+    with open(storage_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+
+
+create_json_file_based_on_name(random_uuid)
