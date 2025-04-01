95f6789a9e84bc04520580aac5aa45d3e90d5287
Jaafar Bendriss <bendrisj2@gmail.com>
2025-04-01 21:54:06 +0200

    cleaning

--- document_state.py
+++ document_state.py
@@ -12,25 +12,3 @@ class Status(Enum):
     PENDING = auto()      # 1
     RUNNING = auto()      # 2
     COMPLETED = auto()    # 3
-
-
-
-
-# Example usage
-
-class Color(Enum):
-    RED = 1
-    GREEN = 2
-    BLUE = 3
-
-# Access by name
-print(Color.RED)           # Color.RED
-print(Color.RED.name)      # 'RED'
-print(Color.RED.value)     # 1
-
-# Iteration
-for color in Color:
-    print(color)           # prints all members
-
-# Membership
-print(Color.RED in Color)  # True
